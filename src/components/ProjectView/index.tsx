31dd45f14f9f7469003e27bb598f81b138101e67
Alisson-Py <alissilva1231@hotmail.com>
2022-07-23 22:00:40 -0300

    feat: create initial Projects components

---
+++ src/components/ProjectView/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import "./styles.css";
+
+const ProjectView = () => {
+    return (
+        <div className="project-view">
+            In constrution...
+        </div>
+    );
+};
+
+export default ProjectView;
